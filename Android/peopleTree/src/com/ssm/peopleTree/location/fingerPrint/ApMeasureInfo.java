7325914c649c05b39a2144fa05a40666d4c5e295
eightco <eightco@naver.com>
2015-02-04 00:02:33 +0900

    1

---
+++ Android/peopleTree/src/com/ssm/peopleTree/location/fingerPrint/ApMeasureInfo.java
@@ -0,0 +1,33 @@
+package com.ssm.peopleTree.location.fingerPrint;
+
+public class ApMeasureInfo{
+	
+	public static final int MINLEVEL = -90;
+	String bssid;
+	String ssid;	
+	int level;
+	public ApMeasureInfo(String bssid,String ssid,int level){
+		this.bssid = bssid;
+		this.ssid = ssid;
+		this.level = level;
+	}
+	
+	public String getBssid(){
+		return bssid;
+	}
+	public String getSsid(){
+		return ssid;
+	}
+	public int getlevel(){
+		return level;
+	}
+	
+	public void reviseLevel(int level){
+		
+		this.level =  (int)Math.round((this.level + level)/(double)2);
+	}
+	
+	
+	
+	
+}
