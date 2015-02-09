fbb65d215558d079b0f584edcff8c7ca7f75278b
lee <eightco@naver.com>
2015-02-10 04:41:46 +0900

    그룹나가기 완료

--- Android/peopleTree/src/com/ssm/peopleTree/dialog/MyMenuDialog.java
+++ Android/peopleTree/src/com/ssm/peopleTree/dialog/MyMenuDialog.java
@@ -1,17 +1,32 @@
 package com.ssm.peopleTree.dialog;
 
 
+import org.json.JSONObject;
+
+import com.android.volley.Response.Listener;
 import com.ssm.peopleTree.R;
 
 
 
 
 
+
+
+
+import com.ssm.peopleTree.application.MyManager;
+import com.ssm.peopleTree.group.GroupManager;
+import com.ssm.peopleTree.network.NetworkManager;
+import com.ssm.peopleTree.network.Status;
+import com.ssm.peopleTree.network.protocol.GroupOutRequest;
+import com.ssm.peopleTree.network.protocol.GroupOutResponse;
+
 import android.app.Dialog;
 import android.content.Context;
+import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MyMenuDialog extends Dialog  {
@@ -19,15 +34,69 @@ public class MyMenuDialog extends Dialog  {
 
 	Button btn1;
 	Button btn2;
+	
+	private Listener<JSONObject> onGroupOutResponse;
+	Context mContext;
 	public MyMenuDialog(Context context) {
 		
 		super(context);
+		mContext = context;
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		this.setContentView(R.layout.dialog_group_mymenu);
+		
+		onGroupOutResponse = new Listener<JSONObject>() {
+
+			@Override
+			public void onResponse(JSONObject arg0) {
+	
+				GroupOutResponse res = new GroupOutResponse(arg0);
+				Status status = res.getStatus();
+				
+				if (res.getStatus() == Status.SUCCESS) {
+
+					Toast.makeText(mContext,"그룹을 나갔습니다.", Toast.LENGTH_SHORT).show();
+				}
+				else {
+					Toast.makeText(mContext,"그룹 나가기가 되지않습니다.", Toast.LENGTH_SHORT).show();
+
+				}
+				MyMenuDialog.this.dismiss();
+				GroupManager.getInstance().update(MyManager.getInstance().getGroupMemberId());
+			}
+		};
+		
+		
+		
+		
 		btn1 =(Button)this.findViewById(R.id.mymenuDialog_btn1);
 		btn1.setText("지역설정");
+		btn1.setOnClickListener(new View.OnClickListener() {
+			 
+            public void onClick(View arg0) {
+            	
+            	
+            	
+            	
+            	
+            }
+        });
 		btn2 =(Button)this.findViewById(R.id.mymenuDialog_btn2);
 		btn2.setText("그룹 나가기");
+		btn2.setOnClickListener(new View.OnClickListener() {
+			 
+            public void onClick(View arg0) {
+            	int myid = MyManager.getInstance().getGroupMemberId();
+
+            	NetworkManager.getInstance().request(new GroupOutRequest(myid), onGroupOutResponse, null);
+                        	
+                        	
+            	
+            	
+            }
+        });
+		
+		
+
 		
 	}
 
