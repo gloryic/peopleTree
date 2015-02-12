ac24634af2c3d4666930c414c721301915104605
주영광 <gloryahop@gmail.com>
2015-02-12 12:58:28 +0900

    메세지 인코딩

--- Android/peopleTree/src/com/ssm/peopleTree/device/DeviceStatusReceiver.java
+++ Android/peopleTree/src/com/ssm/peopleTree/device/DeviceStatusReceiver.java
@@ -1,6 +1,7 @@
 package com.ssm.peopleTree.device;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.location.LocationManager;
@@ -16,6 +17,7 @@ public class DeviceStatusReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         String action = intent.getAction();
+        final String PACKAGE_NAME = "com.ssm.peopleTree";
         
         if(action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
         	// wifi
@@ -50,6 +52,28 @@ public class DeviceStatusReceiver extends BroadcastReceiver {
         	else {
         		DeviceStatus.clear(DeviceStatus.BATTERY_LACK);
         		Log.e("device", "battery normal");
+        	}
+        	
+        }
+        else if(Intent.ACTION_PACKAGE_REMOVED.equals(intent.getAction())){
+        	
+        	/*
+        	ComponentName topActivity = mActivityManager.getRunningTasks(1).get(0).topActivity;
+        	String packageName = topActivity.getPackageName();
+        	String className = topActivity.getClassName();
+        	Log.v(TAG, "packageName" + packageName);
+        	Log.v(TAG, "className" + className);
+        	*/
+        	
+        
+        	if(PACKAGE_NAME.equals(intent.getData().getSchemeSpecificPart())){
+        		
+        		Toast.makeText(context, "ACTION_PACKAGE_REMOVED", Toast.LENGTH_LONG).show();
+        		Log.d("_PACKAGE_OBSERVER_", "!!!!!!!");
+	            Log.d("_PACKAGE_OBSERVER_", "intent : "+"!!");
+	    		Log.d("_PACKAGE_OBSERVER_", "  action = " + intent.getAction()+"!!");
+	    		Log.d("_PACKAGE_OBSERVER_", "  data = " + intent.getData()+"!!");
+	    		
         	}
         	
         }
